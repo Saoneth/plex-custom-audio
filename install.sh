817707bd711ebe3074b5ec73622bbf0d8616f376
Rafał Stańczyk <saoneth@gmail.com>
2020-03-16 05:35:26 +0100

    Fix install script.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 TAG="bins"
 
-#[ ! -e "Plex Transcoder_org" ] && mv "Plex Transcoder" "Plex Transcoder_org"
+[ ! -e "Plex Transcoder_org" ] && mv "Plex Transcoder" "Plex Transcoder_org"
 
 TOKEN="$(curl -s "https://auth.docker.io/token?service=registry.docker.io&scope=repository:saoneth/plex-custom-audio:pull" | cut -d'"' -f4)"
 BLOB="$(curl -s -H "Authorization: Bearer ${TOKEN}" "https://registry.hub.docker.com/v2/saoneth/plex-custom-audio/manifests/${TAG}" | grep '"blobSum":' | cut -d'"' -f4)"
