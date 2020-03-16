6b99fba43c4d6b09e0355ee5595ab756db90322e
Rafał Stańczyk <saoneth@gmail.com>
2020-03-16 18:59:12 +0100

    Cache layer

---
+++ cache.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	_"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/saoneth/goav/avformat"
+	_ "github.com/saoneth/goav/avcodec"
+	_ "github.com/saoneth/goav/avutil"
+)
+
+func main() {
+}
