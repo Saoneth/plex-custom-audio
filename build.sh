265f1fa99966b4c8a4c6c1687ff3e98730f31ca6
Rafał Stańczyk <saoneth@gmail.com>
2020-03-16 20:48:50 +0100

    Fix transcoder error when mapping attachments. Optimize build process. Code cleanup.

--- build.sh
+++ build.sh
@@ -1,20 +1,13 @@
-#!/bin/sh
-if [ "$#" -eq "0" ]; then
-   echo "Usage: $0 linux|windows"
+#!/bin/bash -e
+if [[ "$#" -eq "0" ]]; then
+   echo "Usage: $0 os"
    exit
 fi
-if [ "$1" == "linux" ]; then
-    docker build -t plex-custom-audio:bins -f Dockerfile .
-    docker image save plex-custom-audio:bins | tar x --no-anchored 'layer.tar' -O | tar xvf -
-    docker rmi plex-custom-audio:bins
+N="Dockerfile.${1}"
+if [[ -e "$D" ]]; then
+    N="${D}-${RANDOM}"
+    docker build -t "$N" -f "$D".linux .
+    docker image save "$N" | tar x --no-anchored 'layer.tar' -O | tar xvf -
+    docker rmi "$N"
     exit
 fi
-if [ "$1" == "windows" ]; then
-    docker build -t plex-custom-audio:bins-windows -f Dockerfile-windows .
-    docker image save plex-custom-audio:bins-windows | tar x --no-anchored 'layer.tar' -O | tar xvf -
-    docker rmi plex-custom-audio:bins-windows
-    exit
-fi
-echo "Invalid system."
-exit 1
-
--- transcoder.go
+++ transcoder.go
@@ -47,6 +47,13 @@ func getDBPath() string {
 	return "com.plexapp.plugins.library.db"
 }
 
+func runTranscoder(string[] args) {
+    err := syscall.Exec(args[0] + "_org", args, os.Environ())
+    if err != nil {
+        log.Fatal(err)
+    }
+}
+
 func main() {
 	f, err := os.OpenFile("/tmp/plex-custom-audio.log", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
 	if err != nil {
@@ -66,15 +73,6 @@ func main() {
 	}
 	defer db.Close()
 
-	streams := 0
-	maps := 0
-	path := ""
-	audio_path := ""
-	audio_index := -1
-	audio_codec := ""
-	video_map := ""
-	no_accurate_seek := false
-
 	get_media_by_id_stmt, err := db.Prepare("SELECT `media_item_id` FROM `media_parts` WHERE `id` = ? LIMIT 1")
 	if err != nil {
 		log.Fatal(err)
@@ -93,7 +91,7 @@ func main() {
 	}
 	defer get_media_stream_url_stmt.Close()
 
-	getInfo := func(path string, index int) (audio_path string, audio_index int) {
+	getInfo := func(path string, index int) (audioPath string, audioIndex int) {
 		log.Println("path:", path)
 		var media_part_id int
 		var media_item_id int
@@ -116,190 +114,210 @@ func main() {
 		var url string
 		var url_index int
 		get_media_stream_url_stmt.QueryRow(media_part_id, media_item_id, index).Scan(&url, &url_index)
-		audio_path = url[7:]
-		//audio_index = index - 1000
-		audio_index = url_index
+		audioPath = url[7:]
+		//audioIndex = index - 1000
+		audioIndex = url_index
 
-		return audio_path, audio_index
+		return audioPath, audioIndex
 	}
+
+	inputCounter := 0
+	mapCounter := 0
+	path := ""
+	audioPath := ""
+	audioIndex := -1
+	audioCodec := ""
+	videoMap := ""
+	hasNoAccurateSeek := false
+	seek := ""
 
 	for i := 1; i < len(os.Args); i++ {
 		arg := os.Args[i]
 		if arg == "-no-accurate_seek" {
-			no_accurate_seek = true
-		} else if strings.HasPrefix(arg, "-codec:") {
-			stream, err := strconv.Atoi(arg[7:])
-			if err != nil {
-				log.Fatal(err)
-			}
+			hasNoAccurateSeek = true
+            continue
+		}
+        if arg == "-ss" {
 			i++
-			codec := os.Args[i]
-			log.Println("stream", stream)
-			if stream >= 1000 {
-				audio_codec = codec
+			seek = os.Args[i]
+				continue
 			}
-		} else if arg == "-i" {
+        if strings.HasPrefix(arg, "-codec:") {
 			i++
-			if streams == 0 {
+			streamIndex, err := strconv.Atoi(arg[7:])
+			if err == nil && streamIndex >= 1000 {
+				continue
+			}
+            audioCodec = os.Args[i]
+            log.Printf("audioCodec: %s\n", audioCodec)
+            continue
+		}
+        if arg == "-i" {
+			i++
+			if inputCounter == 0 {
 				path = os.Args[i]
 			}
-			streams++
-		} else if arg == "-map" {
-			maps++
+			inputCounter++
+            continue
+		}
+        if arg == "-map" {
 			i++
-			mapp := os.Args[i]
+			streamMap := os.Args[i]
+			mapCounter++
 
-			if strings.IndexByte(mapp, ':') == -1 {
+			if strings.IndexByte(streamMap, ':') == -1 {
 				continue
 			}
 
-			index, err := strconv.Atoi(strings.Split(mapp, ":")[1])
+			streamIndex, err := strconv.Atoi(strings.Split(streamMap, ":")[1])
 			if err != nil {
-				log.Println(err)
 				continue
 			}
 
-			if index < 1000 {
-				log.Println("skip:", index)
+			if streamIndex < 1000 {
+				log.Printf("skip: inputCounter: %d, path: %s, index: %s\n", inputCounter, path, streamIndex)
 				continue
 			}
 
-			log.Printf("streams: %d\n", streams)
-
-			audio_path, audio_index = getInfo(path, index)
-			log.Printf("audio_path: %s, audio_index: %d\n", audio_path, audio_index)
-		} else if arg == "-filter_complex" {
+			audioPath, audioIndex = getInfo(path, streamIndex)
+			log.Printf("inputCounter: %d, audioPath: %s, audioIndex: %d\n", inputCounter, audioPath, audioIndex)
+            continue
+		}
+        if arg == "-filter_complex" {
 			i++
 			filter_complex := os.Args[i]
 			log.Printf("filter_complex: %s\n", filter_complex)
 
 			j := strings.IndexByte(filter_complex, ']')
-			mapp := filter_complex[1:j]
-			log.Printf("map: %s\n", mapp)
+			streamMap := filter_complex[1:j]
+			log.Printf("filter_complex map: %s\n", streamMap)
 
 			if strings.IndexByte(filter_complex, ':') == -1 {
 				continue
 			}
 
-			index, err := strconv.Atoi(strings.Split(mapp, ":")[1])
+			streamIndex, err := strconv.Atoi(strings.Split(streamMap, ":")[1])
 			if err != nil {
 				// index = 0
 				log.Fatal(err)
 			}
-			audio_index = index
+			audioIndex = streamIndex
 
-			if index >= 1000 {
+			if streamIndex >= 1000 {
 				log.Println("Path:", path)
-				audio_path, audio_index = getInfo(path, index)
+				audioPath, audioIndex = getInfo(path, streamIndex)
 			}
-			log.Printf("audio_path: %s, audio_index: %d\n", audio_path, audio_index)
+			log.Printf("audioPath: %s, audioIndex: %d\n", audioPath, audioIndex)
+            continue
 		}
-//		log.Println(arg)
 	}
 
-	if maps < 2 {
+	if mapCounter < 2 {
 		log.Println("Probably audio streaming")
-		if err := syscall.Exec(os.Args[0] + "_org", os.Args, os.Environ()); err != nil {
-			log.Fatal(err)
-		}
+		runTranscoder(os.Args)
 	}
 
-	log.Printf("audio_path: %s, audio_index: %d\n", audio_path, audio_index)
+	log.Printf("audioPath: %s, audioIndex: %d\n", audioPath, audioIndex)
 
-	args := []string{}
-	ss := ""
-	s := 0
+	args := []string{os.Args[0] + "_org"}
+	currentInputIdx := 0
 
-	for i := 0; i < len(os.Args); i++ {
+	for i := 1; i < len(os.Args); i++ {
 		arg := os.Args[i]
-		if strings.HasPrefix(arg, "-codec:") {
-			stream, err := strconv.Atoi(arg[7:])
-			if err != nil {
-				log.Fatal(err)
-			}
+        
+        // Add -codec only if it's for valid input
+		if strings.HasPrefix(arg, "-codec:") 
 			i++
-			codec := os.Args[i]
+			index, err := strconv.Atoi(arg[7:])
+			if err != nil || index < 1000 {
+				args = append(args, arg, os.Args[i])
+			}
+            continue
+			}
 
-			if stream < 1000 {
-				args = append(args, arg, codec)
-			}
-		} else if arg == "-ss" {
+        // Input flag
+        if arg == "-i" {
 			i++
-			ss = os.Args[i]
-			args = append(args, arg, ss)
-		} else if arg == "-i" {
-			s++
-			i++
+			currentInputIdx++
+
+            // Append flags as they are
 			args = append(args, arg, os.Args[i])
-			if audio_path != "" && s == streams {
-				if audio_codec != "" {
-					args = append(args, fmt.Sprintf("-codec:%d", audio_index))
-					args = append(args, audio_codec)
+            
+            // If it's last not last input - skip
+			if currentInputIdx != inputCounter {
+                continue
 				}
-				if ss != "" {
-					args = append(args, "-ss")
-					args = append(args, ss)
+            
+            // Add codec flag
+            if audioCodec != "" {
+                args = append(args, fmt.Sprintf("-codec:%d", audioIndex), audioCodec)
 				}
-				if no_accurate_seek {
+
+            // Add seeking flag
+            if seek != "" {
+                args = append(args, "-ss", seek)
+            }
+
+            // Add -no_accurate_seek
+            if hasNoAccurateSeek {
 					// With this switch, it's faster, but sometimes at the beginning for a few seconds there is no audio
 					// args = append(args, "-no_accurate_seek")
 				}
-				args = append(args, "-analyzeduration", "20000000", "-probesize", "20000000", "-i", audio_path)
+            
+            // Add new input with audio file
+            args = append(args, "-analyzeduration", "20000000", "-probesize", "20000000", "-i", audioPath)
+            continue
 			}
-		} else if arg == "-map" {
-			i++
-			mapp := os.Args[i]
 
-			log.Printf("map: %s, video_map: %s\n", mapp, video_map)
-			if strings.IndexByte(mapp, ':') != -1 {
-				index, err := strconv.Atoi(strings.Split(mapp, ":")[1])
-				if err != nil {
-					// index = 0
-					log.Println(err)
-				} else {
-					if index >= 1000 {
-						mapp = fmt.Sprintf("%d:%d", streams, audio_index)
+        // Audio identifier could be specified in this flag
+        if arg == "-map" {
+			i++
+			streamMap := os.Args[i]
+
+			log.Printf("streamMap: %s, videoMap: %s\n", streamMap, videoMap)
+			if strings.IndexByte(streamMap, ':') != -1 {
+				index, err := strconv.Atoi(strings.Split(streamMap, ":")[1])
+                // Replace invalid stream identifier for audio with working one
+				if err == nil && index >= 1000 {
+					streamMap = fmt.Sprintf("%d:%d", inputCounter, audioIndex)
 					}
 				}
+
+			args = append(args, arg, streamMap)
+            continue
 			}
 
-			args = append(args, arg, mapp)
-		} else if arg == "-filter_complex" {
+        // Audio identifier could be specified in this flag
+        if arg == "-filter_complex" {
 			i++
 			filter_complex := os.Args[i]
 			log.Printf("filter_complex: %s\n", filter_complex)
 
 			j := strings.IndexByte(filter_complex, ']')
-			mapp := filter_complex[1:j]
-			log.Printf("map: %s\n", mapp)
+			streamMap := filter_complex[1:j]
+			log.Printf("filter_complex map: %s\n", streamMap)
 
-			if audio_path != "" && strings.IndexByte(filter_complex, ':') != -1 {
-				index, err := strconv.Atoi(strings.Split(mapp, ":")[1])
+			if audioPath != "" && strings.IndexByte(filter_complex, ':') != -1 {
+				index, err := strconv.Atoi(strings.Split(streamMap, ":")[1])
 				if err != nil {
 					// index = 0
 					log.Fatal(err)
 				}
 				if index >= 1000 {
-					filter_complex = fmt.Sprintf("[%d:%d]%s", streams, audio_index, filter_complex[j+1:])
+                    // Replace invalid stream identifier for audio with working one
+					filter_complex = fmt.Sprintf("[%d:%d]%s", inputCounter, audioIndex, filter_complex[j+1:])
 				}
 			}
 
 			args = append(args, arg, filter_complex)
-		} else if arg == "-loglevel" || arg == "-loglevel_plex" {
-			args = append(args, arg, "verbose")
-			i++
-		} else {
-			args = append(args, arg)
+            continue
 		}
+        // default
+		args = append(args, arg)
 	}
-
-
-	args[0] = args[0] + "_org"
 
 	log.Println("Custom audio detected. New args:")
 	log.Println(args)
 
-	if err := syscall.Exec(args[0], args, os.Environ()); err != nil {
-		log.Fatal(err)
-	}
+	runTranscoder(args)
 }
